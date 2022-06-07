23e536078479d85bfa060bdb010d82128e71dadb
Punit Lohia <punit.lohia@BLRETV-N59VG8WN.local>
2022-06-07 08:46:27 +0530

    First Project

---
+++ src/com/flipkart/info/FirstProgram.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.flipkart.info;
+
+/**
+ * @author punit.lohia
+ *
+ */
+public class FirstProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My First Java Program");
+
+	}
+
+}
